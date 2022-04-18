2df9baecf5771d58702e81180a32b697280207ec
mervealgi <72434705+mervealgi@users.noreply.github.com>
2022-04-19 00:38:52 +0300

    19.04

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,5 +1,40 @@
 package org.example;
 
+import com.thoughtworks.gauge.AfterScenario;
+import com.thoughtworks.gauge.BeforeScenario;
+import org.apache.commons.lang.StringUtils;
+import org.jsoup.helper.StringUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+
+
 public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeScenario
+    public static void setUp() {
+        String BaseURL = "";
+        DesiredCapabilities capabilities;
+        if (StringUtils.isEmpty(System.getenv("key"))) {
+            capabilities = DesiredCapabilities.chrome();
+            ChromeOptions options = new ChromeOptions();
+            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+            driver = new ChromeDriver(capabilities);
+        }
+        driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS).implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().fullscreen();
+        driver.get(BaseURL);
+    }
+
+    @AfterScenario
+    public void tearDown() throws Exception{
+        driver.quit();
+    }
 
 }
