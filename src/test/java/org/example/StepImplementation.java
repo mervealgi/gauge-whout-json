3b315beb6040051f8d18e023fadc07c94d5a3699
mervealgi <72434705+mervealgi@users.noreply.github.com>
2022-04-19 00:49:53 +0300

    19.04

--- src/test/java/org/example/StepImplementation.java
+++ src/test/java/org/example/StepImplementation.java
@@ -1,32 +1,37 @@
 package org.example;
 
-import com.thoughtworks.gauge.Gauge;
+
+
 import com.thoughtworks.gauge.Step;
-import driver.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StepImplementation {
-    @Step("Go to Gauge Get Started Page")
-    public void gotoGetStartedPage() throws InterruptedException {
-        WebElement getStartedButton = Driver.webDriver.findElement(By.xpath("//a[@href='https://docs.gauge.org/getting_started/installing-gauge.html']"));
-        getStartedButton.click();
+public class StepImplementation extends BaseTest {
 
-        Gauge.writeMessage("Page title is %s", Driver.webDriver.getTitle());
+    @Step("<key> saniye bekle")
+    public void waitSecond(int key) throws InterruptedException{
+        Thread.sleep(key *1000);
     }
 
-    @Step("Ensure installation instructions are available")
-    public void ensureInstallationInstructionsAreAvailable() throws InterruptedException {
-        WebElement instructions = Driver.webDriver.findElement(By.xpath("//a[@href='/writing-specifications.html']"));
-        assertThat(instructions).isNotNull();
+    @Step("<key> xptahli elementine tikla")
+    public void clickElementByXpath(String key){
+        driver.findElement(By.xpath(key)).click();
     }
 
-    @Step("Open the Gauge homepage")
-    public void implementation1() {
-        String app_url = System.getenv("APP_URL");
-        Driver.webDriver.get(app_url + "/");
-        assertThat(Driver.webDriver.getTitle()).contains("Gauge");
+    @Step("<key> cssli elementine tikla")
+    public void clickElementByCss(String key){
+        driver.findElement(By.cssSelector(key)).click();
     }
+
+    @Step("<key> elementi bul ve <text> degerini cssselector ile yazdir")
+    public void SendKeyByCss(String element, String key){
+        driver.findElement(By.cssSelector(element)).sendKeys(key);
+    }
+
+    @Step("<key> elementi bul ve <text> degerini xpath ile yazdir")
+    public void SendKeyByXpath(String element, String key){
+        driver.findElement(By.cssSelector(element)).sendKeys(key);
+    }
+
 }
